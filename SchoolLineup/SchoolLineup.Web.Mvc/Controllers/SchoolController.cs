7ad1265618acfb5c7b76c023bec609b865a9eaee
Douglas Collioni <dcollioni@gmail.com>
2014-03-13 16:53:09 -0300

    Adding School page.

---
+++ SchoolLineup/SchoolLineup.Web.Mvc/Controllers/SchoolController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SchoolLineup.Web.Mvc.Controllers
+{
+    public class SchoolController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
