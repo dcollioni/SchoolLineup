2770e52dbfaca81df4062856a46741d6751ddb93
Douglas Collioni <dcollioni@gmail.com>
2014-12-01 16:50:55 -0200

    Include missing files

---
+++ SchoolLineup/SchoolLineup.Web.Mvc/Controllers/Queries/Teacher/ITeacherListQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolLineup.Web.Mvc.Controllers.Queries.Teacher
+{
+    using SchoolLineup.Web.Mvc.Controllers.ViewModels;
+    using System.Collections.Generic;
+
+    public interface ITeacherListQuery
+    {
+        IEnumerable<TeacherViewModel> GetAll();
+    }
+}
